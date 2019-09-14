9f17a1baa64c82278311d991f7317dba6d4612d7
George Xiao <g5xiao@uwaterloo.ca>
2019-09-14 16:00:48 -0400

    Added similar compound call

--- public/api-service.js
+++ public/api-service.js
@@ -7,4 +7,10 @@ class apiService {
       let data = await response.json()
       return data;
     }
+
+    async getSimilarCompounds(chemblID) {
+      let response = await fetch('https://www.ebi.ac.uk/chembl/api/data/similarity.json?limit=10&offset=0&only=molecule_chembl_id&similarity=85&chembl_id=' + chemblID);
+      let data = await response.json();
+      return data;
+    }
 }
